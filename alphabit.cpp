99c78c13ec2cf38e47822b96a66e21fbb6da64c8
Stephen Johnson <104575665+stephen-three@users.noreply.github.com>
2022-05-02 16:54:40 +0200

    unnecessary note

--- alphabit.cpp
+++ alphabit.cpp
@@ -40,7 +40,6 @@ const std::string notes = "Tested. Working as intended."
 */
 
 #define MAX_SIZE (96000 * 30) // 30 sec of floats at 96 kHz
-// 2,880,000
 #define FREQ_MAX 2000 // 2kHz
 #define FREQ_MIN 50 // 50Hz
 #define FLTR_OFF 1006
